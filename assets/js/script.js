627db402a3a021a71ab3bc4c9006d5590eeac382
Daniel Lucas <danomiterock@yahoo.com>
2021-03-02 18:49:44 -0700

    add div and js for startButton and countDownTimer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,38 @@
+startButton = document.querySelector(".btn")
+countDownTimer = document.querySelector(".timer")
+
 //Listen for click
-document.eventListener("click")startQuiz();
+document.eventListener("click", startQuiz()) {
+
+}
+
+//Time calculations in seconds
+var seconds = Math.floor((distance % (1000 * 120)) / 1000);
+var question1 = {
+
+}
+var question2 = {
+
+}
+
+var question3 = {
+
+}
+
+var question4 = {
+
+}
+
+
+function startQuiz(){
+    //unhide the timer
+    //begin the takeQuiz function
+}
+
+function takeQuiz(){
+    //prompt first question
+    //log results and prompt next question
+    //continue 
+}
+
+startQuiz(console.log("take the quiz"))
